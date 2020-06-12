9936573a613759e59a0a7db1342af8d5a706ca2f
gon <gonbatalb@gmail.com>
2020-06-12 16:22:59 -0300

    initReady

---
+++ src/index.js
@@ -0,0 +1,19 @@
+let express = require("express");
+let app = express();
+let bodyParser = require("body-parser");
+
+const PORT = process.env.port || 3000;
+app.listen(PORT, () => console.info(`Server has started on Port ${PORT}`));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.status(404).send("We think you are lost!");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+});
+
+module.exports = app;
